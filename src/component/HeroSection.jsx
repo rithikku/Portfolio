9e861e40309dc9fb1fe412ed869ff793cf0ce34a
Rithik Kumar <rithikarchadefacilator@gmail.com>
2025-08-23 01:37:13 +0530

    Update HeroSection.jsx

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -2,8 +2,8 @@ const HeroSection = () => {
     return (
         <section className="hero" id="home">
             <div className="hero-content">
-                <h1>Hi, I'm <span>Rithik</span></h1>
-                <p>I turn ideas into interactive, pixel-perfect experiences that live on the Web</p>
+                <h1 className="names">Hi, I'm<span>Rithik</span></h1>
+                <p className="r-about">I turn ideas into interactive, pixel-perfect experiences that live on the Web</p>
                 <a href="#projects" className="btn">View My Work</a>
             </div>
         </section>
